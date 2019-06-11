86f9af7691bb23649d1fc62456cad5c43934cdea
Levchenko <eduard.levchenko@arcelormittal.com>
2019-06-11 10:13:15 +0300

    add rep

--- WebUI/Controllers/api/oilOutcomesController.cs
+++ WebUI/Controllers/api/oilOutcomesController.cs
@@ -40,5 +40,31 @@ namespace WebUI.Controllers.api
                 return NotFound();
             }
         }
+
+        // GET: api/oil/outcomes/start/2019-06-04T07:00:00/stop/2019-06-04T18:59:59
+        [Route("outcomes/start/{start:datetime}/stop/{stop:datetime}")]
+        [ResponseType(typeof(Outcomes))]
+        public IHttpActionResult GetOutcomes(DateTime start, DateTime stop)
+        {
+            try
+            {
+                string sql = "SELECT TOP 1000 [Id] ,[DateCreated] ,[OperatorCreated] ,[OilType] ,[TankNo] ,[TargetMass] ,[TargetVolume] ,[CreatedLevel] ,[CreatedVolume] ,[CreatedMass] " +
+                    ",[CreatedTemp] ,[CreatedDens] ,[CreatedWater] ,[OutcomeType] ,[Receiver] ,[DateStarted] ,[OperatorStarted] ,[Pump_Used] ,[CounterStart] ,[LevelStart] ,[VolumeStart] " +
+                    ",[MassStart] ,[TempStart] ,[DensStart] ,[WaterStart] ,[DateStop] ,[CounterStop] ,[LevelStop] ,[VolumeStop] ,[MassStop] ,[TempStop] ,[DensStop] ,[WaterStop] " +
+                    ",[FLAG_R] ,[LOGIN_EXP] ,[N_POST] ,[TRANSP_FAKT] ,[N_TREB] ,[LGORT] ,[WERKS] ,[sended] ,[OZM_TREB] ,[OZM_BAK] ,[N_POS] FROM [dbo].[Outcomes] " +
+                    "where [DateStarted] >= convert(datetime,'" + start.ToString("yyyy-MM-dd HH:mm:ss") + "',120) and [DateStarted] <= convert(datetime,'" + stop.ToString("yyyy-MM-dd HH:mm:ss") + "',120) order by [DateStarted]";
+                List<Outcomes> list = this.ef_outcomes.Database.SqlQuery<Outcomes>(sql).ToList();
+
+                if (list == null)
+                {
+                    return NotFound();
+                }
+                return Ok(list);
+            }
+            catch (Exception e)
+            {
+                return NotFound();
+            }
+        }
     }
 }
