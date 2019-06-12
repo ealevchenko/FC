c0b0474343083fa2328e74935a0e997d3751f761
Levchenko <eduard.levchenko@arcelormittal.com>
2019-06-12 16:47:12 +0300

    testing productiv

--- WebUI/Controllers/api/oilOutcomesController.cs
+++ WebUI/Controllers/api/oilOutcomesController.cs
@@ -12,50 +12,6 @@ using System.Web.Http.Description;
 namespace WebUI.Controllers.api
 {
     public class Outcomes_report : Outcomes  {
-        //public long Id { get; set; }
-        //public DateTime DateCreated { get; set; }
-        //public string OperatorCreated { get; set; }
-        //public string OilType { get; set; }
-        //public string TankNo { get; set; }
-        //public double TargetMass { get; set; }
-        //public double TargetVolume { get; set; }
-        //public double CreatedLevel { get; set; }
-        //public double CreatedVolume { get; set; }
-        //public double CreatedMass { get; set; }
-        //public double CreatedTemp { get; set; }
-        //public double CreatedDens { get; set; }
-        //public double CreatedWater { get; set; }
-        //public int OutcomeType { get; set; }
-        //public string Receiver { get; set; }
-        //public DateTime? DateStarted { get; set; }
-        //public string OperatorStarted { get; set; }
-        //public string Pump_Used { get; set; }
-        //public long? CounterStart { get; set; }
-        //public double? LevelStart { get; set; }
-        //public double? VolumeStart { get; set; }
-        //public double? MassStart { get; set; }
-        //public double? TempStart { get; set; }
-        //public double? DensStart { get; set; }
-        //public double? WaterStart { get; set; }
-        //public DateTime? DateStop { get; set; }
-        //public long? CounterStop { get; set; }
-        //public double? LevelStop { get; set; }
-        //public double? VolumeStop { get; set; }
-        //public double? MassStop { get; set; }
-        //public double? TempStop { get; set; }
-        //public double? DensStop { get; set; }
-        //public double? WaterStop { get; set; }
-        //public int? FLAG_R { get; set; }
-        //public string LOGIN_EXP { get; set; }
-        //public string N_POST { get; set; }
-        //public string TRANSP_FAKT { get; set; }
-        //public string N_TREB { get; set; }
-        //public string LGORT { get; set; }
-        //public string WERKS { get; set; }
-        //public int sended { get; set; }
-        //public string OZM_TREB { get; set; }
-        //public string OZM_BAK { get; set; }
-        //public int? N_POS { get; set; }
         public string Invent { get; set; }
     }
 
